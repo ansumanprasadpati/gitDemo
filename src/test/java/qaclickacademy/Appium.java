386255c3695c282336319ac628e08cf1ab82f337
ansumanprasadpati <ansumanprasadpati@gmail.com>
2020-05-01 21:04:36 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Appium.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Appium {
+	
+	@Test
+	public void turnOnMobile()
+	{
+		System.out.println("Mobile Started");
+	}
+	
+	@Test
+	public void takePicture()
+	{
+		System.out.println("Camera working");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/RESTAPIAutomation.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPIAutomation {
+	
+	@Test
+	public void openGET()
+	{
+		System.out.println("GET request.");
+	}
+	
+	@Test
+	public void openPUT()
+	{
+		System.out.println("PUT request");
+	}
+
+}
